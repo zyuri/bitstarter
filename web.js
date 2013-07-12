46f539b1dc27ea1727ac5abb2037704cdd0c300c
Yuri Cauwerts <yuri.cauwerts@gmail.com>
2013-07-12 15:50:05 +0200

    using buffer

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var fs = require('fs');
+var buf = require('buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+  buf.write(fs.readFileSync('index.html'));
+  response.send(buf.toString());
   /* response.send('Hello World 2!');*/
 });
 
