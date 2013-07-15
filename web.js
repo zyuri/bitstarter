b76b5ad5f3799193bb26197eaabf4d9dab2f1a15
root <root@coursera.vesalius.groept.be>
2013-07-15 23:18:44 +0200

    assignment 4: page styling using css

--- web.js
+++ web.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 //var buf = require('buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buf = new Buffer(fs.readFileSync('index.html'));
+  buf = new Buffer(fs.readFileSync(htmlfile));
   response.send(buf.toString());
   /* response.send('Hello World 2!');*/
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8181;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
